0b8b55d02023edf88d68c9be64dcc08185989b22
TZ001118 <teoruize044@gmail.com>
2025-03-16 00:18:41 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -26,6 +26,6 @@ io.on("connection", (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+server.listen(PORT,"0,0,0,0", () => {
     console.log(`服务器运行在 http://localhost:${PORT}`);
 });
